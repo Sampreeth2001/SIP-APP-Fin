e576401616bb7575c523567a829c31160545d8dd
Sampreeth Shetty <159445305+Sampreeth2001@users.noreply.github.com>
2024-08-03 10:33:15 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,9 @@ if st.button("Download Data"):
         st.write("Data downloaded successfully!")
         st.dataframe(data)
 
+        # Impute missing values with mean
+        data['Adj Close'].fillna(data['Adj Close'].mean(), inplace=True)
+
         # Preprocessing data for LSTM
         scaler = MinMaxScaler(feature_range=(0, 1))
         scaled_data = scaler.fit_transform(data['Adj Close'].values.reshape(-1, 1))
